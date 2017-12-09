c472397850fc85b8f5cec95d1c67ace002259777
opsb <oliver@opsb.co.uk>
2017-12-09 12:18:22 +0000

    Spacing

--- lib/bitmap_editor.rb
+++ lib/bitmap_editor.rb
@@ -21,30 +21,39 @@ class BitmapEditor
         case command
           when /I ([1-9][0-9]*) ([1-9][0-9]*)/
             @bitmap = Bitmap.new(width: $1.to_i, height: $2.to_i)
+
           when /I/
             fail_with_usage!(command, "I M N - Create a new M x N image with all pixels coloured white (O)")
+
           when /L ([1-9][0-9]*) ([1-9][0-9]*) ([A-Z])/
             when_bitmap do
               @bitmap.color_pixel(x: $1.to_i, y: $2.to_i, color: $3)
             end
+
           when /L/
             fail_with_usage!(command, "L X Y C - Colours the pixel (X,Y) with colour C")
+
           when /V ([1-9][0-9]*) ([1-9][0-9]*) ([1-9][0-9]*) ([A-Z])/
             when_bitmap do
               @bitmap.draw_vertical_segment(x: $1.to_i, y1: $2.to_i, y2: $3.to_i, color: $4)
             end
+
           when /V/
             fail_with_usage!(command, "V X Y1 Y2 C - Draw a vertical segment of colour C in column X between rows Y1 and Y2 (inclusive)")
+
           when /H ([1-9][0-9]*) ([1-9][0-9]*) ([1-9][0-9]*) ([A-Z])/
             when_bitmap do
               @bitmap.draw_horizontal_segment(x1: $1.to_i, x2: $2.to_i, y: $3.to_i, color: $4)
             end
+
           when /H/
             fail_with_usage!(command, "H X1 X2 Y C - Draw a horizontal segment of colour C in row Y between columns X1 and X2 (inclusive).")
+
           when 'S'
             when_bitmap do
               @out.puts @bitmap.to_s
             end
+
           else
             raise "unrecognised command :("
         end
